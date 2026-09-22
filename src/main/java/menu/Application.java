0ac93e0f6025cc016ee5074bf3e2748d000751fc
RedSunSmile <111857426+RedSunSmile@users.noreply.github.com>
2026-09-22 21:30:44 +0900

    feat: 코치 이름과 못 먹는 메뉴 입력 기능 구현

--- src/main/java/menu/Application.java
+++ src/main/java/menu/Application.java
@@ -1,7 +1,14 @@
 package menu;
 
+import menu.ui.InputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputView inputView=new InputView();
+        String coach1=inputView.inputCoachNameOfSelectedLunchMenu();
+        String[] coaches=coach1.split(",");
+        for(String coach2:coaches) {
+            inputView.inputKindsOfNotSelectedLunchMenu(coach2.trim());
+        }
     }
 }
--- src/main/java/menu/ui/InputView.java
+++ src/main/java/menu/ui/InputView.java
@@ -1,16 +1,20 @@
 package menu.ui;
 import camp.nextstep.edu.missionutils.Console;
+
+import java.util.List;
+
 public class InputView {
 
     public String inputCoachNameOfSelectedLunchMenu(){
         System.out.println("점심 메뉴 추천을 시작합니다.");
         System.out.println();
-        System.out.println("코치의 이름을 입력해 주세요.(, 로 구분)");
+        System.out.println("코치의 이름을 입력해 주세요. (, 로 구분)");
         return Console.readLine();
     }
 
-    public String inputKindsOfNotSelectedLunchMenu(String name){
-        System.out.println(name+"(이)가 못먹는 메뉴를 입력해 주세요.");
-        return Console.readLine();
+    public List<String> inputKindsOfNotSelectedLunchMenu(String name){
+        System.out.println(name+"(이)가 못 먹는 메뉴를 입력해 주세요.");
+       String input=Console.readLine();
+        return List.of(input.split(", "));
     }
 }
