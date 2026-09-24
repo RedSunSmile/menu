204f416766bdaadfa78a130250bda1dab9f0662b
RedSunSmile <111857426+RedSunSmile@users.noreply.github.com>
2026-09-24 20:39:39 +0900

    feat: 코치 생성 시 검증이 실행되도록 연결하고 빈 입력 처리 추가

--- src/main/java/menu/ui/InputView.java
+++ src/main/java/menu/ui/InputView.java
@@ -1,20 +1,21 @@
 package menu.ui;
-import camp.nextstep.edu.missionutils.Console;
 
+import camp.nextstep.edu.missionutils.Console;
 import java.util.List;
 
 public class InputView {
 
-    public String inputCoachNameOfSelectedLunchMenu(){
+    public String inputCoachNameOfSelectedLunchMenu() {
         System.out.println("점심 메뉴 추천을 시작합니다.");
         System.out.println();
         System.out.println("코치의 이름을 입력해 주세요. (, 로 구분)");
         return Console.readLine();
     }
 
-    public List<String> inputKindsOfNotSelectedLunchMenu(String name){
-        System.out.println(name+"(이)가 못 먹는 메뉴를 입력해 주세요.");
-       String input=Console.readLine();
-        return List.of(input.split(", "));
+    public List<String> inputKindsOfNotSelectedLunchMenu(String name) {
+        System.out.println(name + "(이)가 못 먹는 메뉴를 입력해 주세요.");
+        String input = Console.readLine();
+        if (input.isBlank()) return List.of();
+        return List.of(input.split(","));
     }
 }
