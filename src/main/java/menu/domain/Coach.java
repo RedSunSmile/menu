a3c83c14e7fe082d0d540c81009ff6d2a8741a85
RedSunSmile <111857426+RedSunSmile@users.noreply.github.com>
2026-09-25 21:17:04 +0900

    feat: 코치 목록 인원 검증 연결 및 못 먹는 메뉴 공백 제거

--- src/main/java/menu/domain/Coach.java
+++ src/main/java/menu/domain/Coach.java
@@ -1,37 +1,36 @@
 package menu.domain;
 
-import java.util.Map;
+import java.util.List;
 
 public class Coach {
     private final String name;
-    private final int counts;
-    public Coach(String name, int counts) {
-        validateAvoidMenu(counts);
+    private final List<String> counts;
+    public Coach(String name, List<String> counts) {
         this.name = name;
-        limitedNameLength();
         this.counts = counts;
-
+        validateAvoidMenu(counts);
+        limitedNameLength(name);
     }
 
     public String takeName() {
         return name;
     }
 
-    public int takeCounts() {
+    public List<String> takeCounts() {
         return counts;
     }
 
-    public void limitedNameLength(){
+    public void limitedNameLength(String name){
 
-        boolean b = !(name.length() >= 2 && name.length() <= 4);
-        if (b) {
+        boolean isInvalidLength = !(name.length() >= 2 && name.length()<= 4);
+        if (isInvalidLength) {
                 throw new IllegalArgumentException("[ERROR] 코치 이름은 2~4글자 사이여야 합니다.");
         }
     }
 
-    public void validateAvoidMenu(int values){
-        boolean c = !(values <= 2 );
-       if(c){
+    public void validateAvoidMenu(List<String> values){
+        boolean avoidMenuSize = !(values.size() <= 2 );
+       if(avoidMenuSize){
            throw new IllegalArgumentException("[ERROR] 각 코치는 최대 2개까지 못 먹는 메뉴가 있습니다.");
        }
     }
